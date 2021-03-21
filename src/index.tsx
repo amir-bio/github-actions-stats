af1f5d24fddca1dc9aab8573f94329bc28ba60ab
Amirhossein Andohkosh <4315615+amirha97@users.noreply.github.com>
2021-03-21 13:21:04 +0000

    Install and setup chakra UI

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { ChakraProvider } from "@chakra-ui/react"
+
+import { extendTheme } from "@chakra-ui/react"
+
+const colors = {
+    brand: {
+        900: "#1a365d",
+        800: "#153e75",
+        700: "#2a69ac",
+    },
+}
+const theme = extendTheme({colors})
+
+const App = () => <>Hello </>
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <ChakraProvider theme={theme}>
+            <App/>
+        </ChakraProvider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
