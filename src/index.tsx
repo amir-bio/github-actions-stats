be229447e620a3b8dcf22b58aa549b131351911e
Amirhossein Andohkosh <4315615+amirha97@users.noreply.github.com>
2021-03-21 15:42:55 +0000

    Add histogram charts with basic stats for last 100 runs

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import { Box, Button, Center, ChakraProvider, Flex } from "@chakra-ui/react"
+import { Box, Button, Center, ChakraProvider, Flex, HStack } from "@chakra-ui/react"
 import { Octokit } from "@octokit/rest";
 import { extendTheme } from "@chakra-ui/react"
 import { Input } from "@chakra-ui/react"
+import { VictoryAxis, VictoryChart, VictoryHistogram, VictoryLabel, VictoryVoronoiContainer } from 'victory';
 
 const octokit = new Octokit({
     auth: process.env.REACT_APP_GITHUB_TOKEN
@@ -22,6 +23,7 @@ const App = () => {
     // const [owner, setOwner] = useState("")
     // const [repo, setRepo] = useState("")
     // const [workflowName, setWorkflowName] = useState("")
+    const [workflowRunsStats, setWorkflowRunsStats] = useState<any>({})
 
     // TODO: type properly
     const handleSubmit = async (event: any) => {
@@ -55,37 +57,115 @@ const App = () => {
                     failure: [] as number[],
                     cancelled: [] as number[],
                     startup_failure: [] as number[],
-                }
+                },
+                earliestRun: new Date(8640000000000000).getTime(),
+                latestRun: new Date(-8640000000000000).getTime()
             }
             // only count completed runs
             for (const run of specificWorkflowRuns.workflow_runs) {
-                if (run.status != "completed") continue
+                if (run.status !== "completed") continue
                 stats.conclusion[run.conclusion] += 1
 
-                const durationMs = Date.parse(run.updated_at) - Date.parse(run.created_at)
+                const createdAtTime = Date.parse(run.created_at)
+                const updatedAtTime = Date.parse(run.updated_at)
+                const durationMs = updatedAtTime - createdAtTime
                 stats.durations[run.conclusion].push(durationMs / 1000)
+
+                stats.earliestRun = Math.min(stats.earliestRun, createdAtTime)
+                stats.latestRun = Math.max(stats.latestRun, createdAtTime)
+
             }
 
             console.log(stats)
+
+            setWorkflowRunsStats(stats)
         } catch (e) {
             console.error("error while getting data from github", e)
         }
 
     }
     return (
-        <Box display="flex" maxW="1140px" mx="auto" pt={50}
-             justifyContent={"center"}>
-            <Flex>
+        <Box display="flex"
+             maxW="1440px"
+             mx="auto"
+             pt={50}
+             justifyContent={"center"}
+             flexDirection="column"
+        >
+            <Flex direction="row">
                 <form onSubmit={handleSubmit}>
-                    <Input placeholder="Repo owner (organisation)" name="owner"/>
-                    <Input placeholder="Name of the repo" name="repo"/>
-                    <Input placeholder="Name of the workflow (e.g. main.yml)" name="workflowName"
-                    />
-                    <Center pt={5}>
-                        <Button type="submit">Visualize!</Button>
-                    </Center>
+                    <HStack spacing="30px">
+                        <Input placeholder="Repo owner (organisation)" name="owner"/>
+                        <Input placeholder="Name of the repo" name="repo"/>
+                        <Input placeholder="Name of the workflow (e.g. main.yml)" name="workflowName"/>
+
+                        <Button type="submit" w={80}>Visualize!</Button>
+                    </HStack>
+
                 </form>
             </Flex>
+            {workflowRunsStats?.durations?.success && workflowRunsStats?.totalRuns && (
+                <Flex>
+
+                    Total Runs: {workflowRunsStats.totalRuns} <br/>
+                    {workflowRunsStats.conclusion.success} Successes <br/>
+                    {workflowRunsStats.conclusion.failure} Failures <br/>
+                    {workflowRunsStats.conclusion.cancelled} Cancelled <br/>
+                    {workflowRunsStats.conclusion.startup_failure} Start up failure <br/>
+                    <br/>
+                    Latest Run: {new Date(workflowRunsStats.latestRun).toLocaleDateString()} <br/>
+                    Earliest Run: {new Date(workflowRunsStats.earliestRun).toLocaleDateString()} <br/>
+
+                </Flex>
+            )}
+
+            <Flex>
+                {workflowRunsStats?.durations?.success && (
+                    <VictoryChart
+                        domainPadding={10}
+                        width={1000}
+                        height={300}
+                        containerComponent={
+                            <VictoryVoronoiContainer
+                                labels={({datum}) => `${datum.y} (${datum.x} minutes)`}
+                            />
+                        }
+                    >
+
+                        <VictoryLabel
+                            x={500}
+                            y={25}
+                            textAnchor="middle"
+                            text="Duration of successful runs"
+                        />
+
+                        <VictoryAxis
+                            dependentAxis
+                            label="Total number of runs"
+                        />
+
+                        <VictoryAxis
+                            label="Time (minutes)"
+                        />
+
+                        <VictoryHistogram
+                            style={{data: {fill: "#29d05c"}}}
+                            binSpacing={20}
+                            bins={50}
+                            // data must be in this format: [ {x: t1}, {x: t2}, ... ]
+                            // also convert second duration to minutes
+                            data={
+                                workflowRunsStats.durations.success.map(successDuration => ({
+                                    x: successDuration / 60
+                                }))
+
+                            }
+
+                        />
+                    </VictoryChart>
+                )}
+            </Flex>
+
         </Box>
     )
 }
